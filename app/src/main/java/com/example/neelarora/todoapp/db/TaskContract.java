574c82cd558f2beed0549cafd92c648ca98b2bb6
Neel Arora <ntarora@vt.edu>
2016-11-17 12:58:06 -0500

    initial commit with barebone todo app

---
+++ app/src/main/java/com/example/neelarora/todoapp/db/TaskContract.java
@@ -0,0 +1,19 @@
+package com.example.neelarora.todoapp.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * holds the DB code for task creation
+ * Created by neelarora on 11/16/16.
+ */
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.neelarora.todoapp.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
