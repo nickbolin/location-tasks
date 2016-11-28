fbebb36e05369ad7e10e42fe61ced8c348e85c75
Neel Arora <ntarora@vt.edu>
2016-11-28 11:50:16 -0500

    one test for adding a geofence

--- app/src/androidTest/java/com/example/neelarora/todoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/neelarora/todoapp/ExampleInstrumentedTest.java
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -22,5 +22,27 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.neelarora.todoapp", appContext.getPackageName());
+    }
+
+    @Test
+    public  void test_addGeofence() throws Exception{
+
+
+        //context = new MockContext();
+        MainActivity main = new MainActivity();
+        Context context =  main.getApplicationContext();
+        GeofenceController.getInstance().init(context);
+        GeofenceController controller = GeofenceController.getInstance();
+
+
+        NamedGeofence geofence = new NamedGeofence();
+        geofence.name = "TEST";
+        geofence.latitude = 30.00;
+        geofence.longitude = 30.00;
+        geofence.radius = (float) 0x1.4p2;
+
+
+        controller.addGeofence(geofence, main.geofenceControllerListener);
+        assertEquals(1, controller.getNamedGeofences().size());
     }
 }
