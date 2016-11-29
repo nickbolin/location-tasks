5f1d7a55a9bdf8bfad5e7e5ae60d26316c4684e0
Rjakec21 <Rjakec21@vt.edu>
2016-11-29 11:18:34 -0500

    Added unit tests for geofencing out of bounds

--- app/src/test/java/com/example/neelarora/todoapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/neelarora/todoapp/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.example.neelarora.todoapp;
 
 import android.view.MenuItem;
 
+import com.google.android.gms.location.Geofence;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,13 +15,159 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test_getGeofenceController() throws Exception {
+        GeofenceController cont = new GeofenceController();
+        assertNotNull(cont.getInstance());
     }
 
     @Test
-    public void test_editTask() throws Exception {
-        MainActivity main = new MainActivity();
-        main.onOptionsItemSelected(MenuItem.class.newInstance().setChecked(true));
+    public void test_LatMin()   {
+        Constants.Geometry geo = new Constants.Geometry();
+        assertTrue(geo.MinLatitude == -90.0);
+    }
+
+    @Test
+    public void test_LatMax()   {
+        Constants.Geometry geo = new Constants.Geometry();
+        assertTrue(geo.MaxLatitude == 90.0);
+    }
+
+    @Test
+    public void test_LonMin()   {
+        Constants.Geometry geo = new Constants.Geometry();
+        assertTrue(geo.MinLongitude == 180.0);
+    }
+
+    @Test
+    public void test_LonMax()   {
+        Constants.Geometry geo = new Constants.Geometry();
+        assertTrue(geo.MaxLongitude == -180.0);
+    }
+
+    @Test
+    public void test_RadMin()   {
+        Constants.Geometry geo = new Constants.Geometry();
+        assertTrue(geo.MinRadius == 0.01);
+    }
+
+    @Test
+    public void test_RadMax()   {
+        Constants.Geometry geo = new Constants.Geometry();
+        assertTrue(geo.MaxRadius == 20.0);
+    }
+
+    @Test
+    public void test_SharedPrefs()    {
+        Constants.SharedPrefs pref = new Constants.SharedPrefs();
+        assertTrue(pref.Geofences.equals("SHARED_PREFS_GEOFENCES"));
+    }
+
+    @Test
+    public void test_AreWeThereIntentService()  {
+        AreWeThereIntentService serv = new AreWeThereIntentService();
+        assertNotNull(serv);
+    }
+
+    @Test
+    public void test_geofence_zeroRadius() throws Exception {
+        NamedGeofence fence = new NamedGeofence();
+        fence.name = "Test Location";
+        fence.latitude = 37.2296;
+        fence.longitude = -80.4139;
+        fence.radius = (float)0.0;
+
+        try   {
+            Geofence result = fence.geofence();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+        }
+
+    }
+
+    @Test
+    public void test_geofence_negativeRadius() throws Exception {
+        NamedGeofence fence = new NamedGeofence();
+        fence.name = "Test Location";
+        fence.latitude = 37.2296;
+        fence.longitude = -80.4139;
+        fence.radius = (float)-5.0;
+
+        try   {
+            Geofence result = fence.geofence();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void test_geofence_HighLong() throws Exception {
+        NamedGeofence fence = new NamedGeofence();
+        fence.name = "Test Location";
+        fence.latitude = 37.2296;
+        fence.longitude = 181.0;
+        fence.radius = (float)5.0;
+
+        try   {
+            Geofence result = fence.geofence();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void test_geofence_LowLong() throws Exception {
+        NamedGeofence fence = new NamedGeofence();
+        fence.name = "Test Location";
+        fence.latitude = 37.2296;
+        fence.longitude = -181.0;
+        fence.radius = (float)5.0;
+
+        try   {
+            Geofence result = fence.geofence();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void test_geofence_LowLat() throws Exception {
+        NamedGeofence fence = new NamedGeofence();
+        fence.name = "Test Location";
+        fence.latitude = -91;
+        fence.longitude = -80.4139;
+        fence.radius = (float) 5.0;
+
+        try   {
+            Geofence result = fence.geofence();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void test_geofence_HighLat() throws Exception {
+        NamedGeofence fence = new NamedGeofence();
+        fence.name = "Test Location";
+        fence.latitude = 91;
+        fence.longitude = -80.4139;
+        fence.radius = (float) 5.0;
+
+        try   {
+            Geofence result = fence.geofence();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+        }
     }
 }
